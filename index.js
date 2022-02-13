502bac251f7f6d466b7e067fd9e653f82dfe6992
Steve-Ndicunguye <ndicunguyesteve4@gmail.com>
2022-02-13 04:02:31 -0800

    Enabled Access-Control-Allow-Origin

--- index.js
+++ index.js
@@ -69,10 +69,13 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs))
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
 });
 
 
