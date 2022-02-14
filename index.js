8f7ac862f7cf2742c3ea845a15a991dc6eea2af6
Steve-Ndicunguye <ndicunguyesteve4@gmail.com>
2022-02-13 22:47:34 -0800

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const options = {
     definition: {
       openapi: "3.0.0",
       info: {
-        title: "My Portfolio Rest API",
+        title: "My Portfolio API",
         version: "1.0.0",
         description: "A simple Express Rest API"
       },
