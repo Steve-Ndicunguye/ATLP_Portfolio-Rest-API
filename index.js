b2e725eef52198b418864f33f3480affa550a8ea
Steve-Ndicunguye <ndicunguyesteve4@gmail.com>
2022-02-13 21:56:26 -0800

    Added Access-Control-Allow-Origin

--- index.js
+++ index.js
@@ -71,13 +71,10 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs))
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    next();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 
