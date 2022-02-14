0c103002e3434c96a73a1c166b1011924d090ee4
Steve-Ndicunguye <ndicunguyesteve4@gmail.com>
2022-02-14 06:48:43 -0800

    Update dbConn.js

--- config/dbConn.js
+++ config/dbConn.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(mongodb+srv://ndicunguyesteve4:diderotsteve@cluster0.9vkqx.mongodb.net/steve_db?retryWrites=true&w=majority, { useNewUrlParser: true })
+        await mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true })
     } catch (error) {
         console.error(error)
     }
