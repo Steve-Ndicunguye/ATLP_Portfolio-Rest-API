cd533f5960247892144ac5c9f910ca5e78c8173d
Steve-Ndicunguye <ndicunguyesteve4@gmail.com>
2022-02-14 21:34:25 -0800

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const options = {
       },
       servers: [
         {
-          url: "http://localhost:5000",
+          url: "http://localhost:5000", 
           description: "Development server" 
         },
         {
