a89d1c3bb294a649d300c13e5a37b19665648354
Steve-Ndicunguye <ndicunguyesteve4@gmail.com>
2022-02-15 22:57:49 -0800

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,15 +4,59 @@ import bcrypt from 'bcrypt';
 import joi from '@hapi/joi';
 
 const schema = joi.object({
-        email: joi.string().required().email(),
-        password: joi.string().min(5).required()
+        email: joi.string().required().email().message({"string.pattern.base":"Invalid email",
+                "string.required":"Email is required", "string.empty":"Email field can not be empty",
+                "string.email":"Invalid email"}),
+        password: joi.string().min(5).required().message({"string.pattern.base":"Invalid password",
+                "string.required":"Password is required", "string.empty":"Password field can not be empty",
+                "string.min":"Password length can not be less than 5 characters"})
     });
 
 //test
 const handleLogin = async (req, res) => {
 
     const {error} = schema.validate(req.body);
-    if (error) return res.send(error.details[0].message);
+    if (error) return res.send(error.message: {
+      'any.custom': [Object],
+      'any.default': [Object],
+      'any.failover': [Object],
+      'any.invalid': [Object],
+      'any.only': [Object],
+      'any.ref': [Object],
+      'any.required': [Object],
+      'any.unknown': [Object],
+      'string.alphanum': [Object],
+      'string.base': [Object],
+      'string.base64': [Object],
+      'string.creditCard': [Object],
+      'string.dataUri': [Object],
+      'string.domain': [Object],
+      'string.email': [Object],
+      'string.empty': [Object],
+      'string.guid': [Object],
+      'string.hex': [Object],
+      'string.hexAlign': [Object],
+      'string.hostname': [Object],
+      'string.ip': [Object],
+      'string.ipVersion': [Object],
+      'string.isoDate': [Object],
+      'string.isoDuration': [Object],
+      'string.length': [Object],
+      'string.lowercase': [Object],
+      'string.max': [Object],
+      'string.min': [Object],
+      'string.normalize': [Object],
+      'string.token': [Object],
+      'string.pattern.base': [Object],
+      'string.pattern.name': [Object],
+      'string.pattern.invert.base': [Object],
+      'string.pattern.invert.name': [Object],
+      'string.trim': [Object],
+      'string.uri': [Object],
+      'string.uriCustomScheme': [Object],
+      'string.uriRelativeOnly': [Object],
+      'string.uppercase': [Object]
+});
     
     //check if a email is in the database
     const user = await User.findOne({email: req.body.email});
