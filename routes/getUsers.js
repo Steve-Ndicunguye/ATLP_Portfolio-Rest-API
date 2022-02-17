216418bc8447a835ccaaa295b0fc364f46d65e8c
Steve-Ndicunguye <ndicunguyesteve4@gmail.com>
2022-02-17 06:54:42 -0800

    Update getUsers.js

--- routes/getUsers.js
+++ routes/getUsers.js
@@ -1,7 +1,28 @@
 import express from 'express';
 const router = express.Router();
 import getUsersController from '../controllers/getUsersController.js';
+import protectRoute from '../middleware/verifyToken.js';
 
-router.get('/', getUsersController.handleGetUsers)
+/**
+ * @swagger
+ * /getAllUsers:
+ *   get:
+ *     summary: Get all users
+ *     tags: [Authentication]
+ *     responses:
+ *       200:
+ *         description: All signed up users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       500:
+ *         description: Internal server error
+ *       401:
+ *         description: Unauthorized   
+*/       
+router.route('/')
+.get( protectRoute.authAdmin , getUsersController.handleGetUsers) 
+
     
 export default router;
